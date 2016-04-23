e3fc4400bae4450ad07b90c379161ac746e25c62
IppeiMUKAIDA <mk_mkee@ippei-mac.local>
2016-04-23 22:41:52 +0900

    Fixed bug and Google Image Search was commented out

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-var title_e=document.getElementById("player-song-title");
+var title_e=document.getElementById("currently-playing-title");
 var title=title_e.innerHTML; 
 title=title.replace(/& /g,"&");
 var tenc=encodeURIComponent(title);
@@ -11,12 +11,13 @@ var album=album_e[0].innerHTML;
 album=album.replace(/& /g,"&"); 
 var tweet=encodeURIComponent('Title : '+title+'\n')+encodeURIComponent('Artist : '+artist+'\n')+encodeURIComponent('Album : '+album+'\n\n'); 
 
-var google = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=";
-var search= new XMLHttpRequest();
-search.open("GET", google+title+artist, false);
-search.send();
-var result = search.responseText;
-var url = result.match( /(http?:\/\/[\x21\x23-\x2B\x2D-\x7e]+)/ );
-console.log(url[0]);
+// var google = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=";
+// var search= new XMLHttpRequest();
+// search.open("GET", google+title+artist, false);
+// search.send();
+// var result = search.responseText;
+// var url = result.match( /(http?:\/\/[\x21\x23-\x2B\x2D-\x7e]+)/ );
+// console.log(url[0]);
 
-void(window.open('https://twitter.com/intent/tweet?text='+tweet+url[0]+'&hashtags=NowPlaying'));
+// void(window.open('https://twitter.com/intent/tweet?text='+tweet+url[0]+'&hashtags=NowPlaying'));
+void(window.open('https://twitter.com/intent/tweet?text='+tweet+'&hashtags=NowPlaying'));
